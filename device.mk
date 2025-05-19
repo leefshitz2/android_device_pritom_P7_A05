9ac9f05ba5285660c10438483144cb1a752fb8b1
leefshitz2 <leefshitz2@gmail.com>
2025-05-19 14:51:07 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
     bootctrl.saturn
 
 PRODUCT_PACKAGES += \
