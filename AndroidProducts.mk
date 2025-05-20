b3059af077e5c74ec899d4706c6551ec9ae180d4
leefshitz2 <leefshitz2@gmail.com>
2025-05-20 14:31:42 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,10 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pb_P7_A05.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/pb_P7_A05.mk
 
-COMMON_LUNCH_CHOICES := \
-    pb_P7_A05-user \
-    pb_P7_A05-userdebug \
-    pb_P7_A05-eng
+COMMON_LUNCH_CHOICES := pb_P7_A05-eng
+
