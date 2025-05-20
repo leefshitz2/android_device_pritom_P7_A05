be1976392f393f06ee3bd9adee31d8ef2824e484
leefshitz2 <leefshitz2@gmail.com>
2025-05-20 15:34:11 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ./bootable/recovery/gui/theme/common/languages/
-rm -f !("en.xml"|"ru.xml")
+rm -f cz.xml de.xml el.xml es.xml fr.xml hu.xml in.xml it.xml ja.xml ko.xml nl.xml pl.xml pt_BR.xml pt_PT.xml sk.xml sl.xml sv.xml tr.xml zh_CN.xml zh_TW.xml
 cd ~-
